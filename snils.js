1654c404e57bd8f6686f6708dd8c85598e968c61
tomatem <tem7720@gmail.com>
2023-03-20 22:58:05 +0300

    done

--- snils.js
+++ snils.js
@@ -3,48 +3,47 @@
 
 // Определи, есть ли во входной строке номер СНИЛС.
 function hasSNILS(string) {
-    let regExp = /\d{3}-\d{3}-\d{3}\s\d{2}(\D|$)/;
+    let regExp = /\D\d{3}-\d{3}-\d{3}\s\d{2}(\D|$)/;
     return regExp.test(string)
 }
 
 // Найди и верни номер СНИЛС из строки.
 function grabSNILS(string) {
-    let regExp = /\d{3}-\d{3}-\d{3}\s\d{2}(\D|$)/;
+    let regExp = /\D\d{3}-\d{3}-\d{3}\s\d{2}(\D|$)/;
     let newstring = regExp.exec(string).join("");
-    let snils = newstring.slice(0, 14);
+    let snils = newstring.slice(1, 15);
     return snils;
 }
 
 // Найди и верни все номера СНИЛС, что есть в строке.
-// function grabAllSNILS(string) {
-//     let regExp = /\d{3}-\d{3}-\d{3}\s\d{2}/g;
-//     let snils = string.match(regExp);
-//     return snils;
-// }
-
 function grabAllSNILS(string) {
-    let regExp = /\d{3}-\d{3}-\d{3}\s\d{2}(\D|$)/g;
+    let regExp = /\D\d{3}-\d{3}-\d{3}\s\d{2}(\D|$)/g;
     let arr = string.match(regExp);
         if (arr === null) {return null}
         else { let snils = [];
             arr.forEach(el => {
-                let s = el.slice(0, 14);
+                let s = el.slice(1, 15);
                 snils.push(s);
             });
-            return snils;}
-    }
+            return snils;
+        }
+}
 
 //console.log(grabAllSNILS("The numbers are 350-802-074 94, 234-600-142 22, and 013-605-876 94"))
 
 // Зашифруй номера СНИЛС. Example: XXX-XXX-XXX 30.
 function hideAllSNILS(string) {
-    let regExp = /(\d{3}-\d{3}-\d{3})\s(\d{2})(\D|$)/g;
-    return string.replace(regExp, `XXX-XXX-XXX $2$3`)
+    let regExp = /\D(\d{3}-\d{3}-\d{3})\s(\d{2})(\D|$)/g;
+    return string.replace(regExp, ` XXX-XXX-XXX $2$3`)
 }
 
 // Отформатируй все номера СНИЛС, чтобы использовались правильные разделители:
 // '48001443027', '480.014.430.27', и '480--014--430 27' должны превратиться в '480-014-430 27'.
-function formatSNILS(string) {}
+
+function formatSNILS(string) {
+    let regexp = /(\D)(\d{3})\D*?(\d{3})\D*?(\d{3})\D*?(\d{2})(\D|$)/g
+    return string.replace(regexp, '$1$2-$3-$4 $5$6')
+}
 
 module.exports = {
   hasSNILS,
